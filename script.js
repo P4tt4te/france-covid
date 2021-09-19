5634829ce54ae873e2b7dff1ee826fa3e171416e
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-09-19 20:35:59 +0200

    final pc test
    -mise en place du footer (liens)
    -fintion de l'api
    -css en version pc
    
    // faire la version mobile

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
 var cas;
 var morts;
 var hospital;
+var date;
+var annee;
+var mois;
+var jour;
 
 var boxcas = document.querySelector('.boxcas>span');
 var boxmorts = document.querySelector('.boxmorts>span');
 var boxhospital = document.querySelector('.boxhospital>span');
+var boxdate = document.querySelector('.maj>span');
+
+window.addEventListener('load',requete);
+
+function requete(evt) {
+	fetch("https://covid-19-data.p.rapidapi.com/country?name=france", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+			"x-rapidapi-key": "2d024dd615msh1eab2c47260e3bfp1b9a61jsn10d150aa6b43"
+		}
+	})
+	.then(response => {
+		console.log(response);
+	  return response.json();
+	})
+	.then(function(object) {
+	  console.log(object);
+	  cas = object[0].confirmed;
+	  boxcas.textContent = cas.toLocaleString();
+	  morts = object[0].deaths;
+	  boxmorts.textContent = morts.toLocaleString();
+	  hospital = object[0].critical;
+	  boxhospital.textContent = hospital.toLocaleString();
+		date = object[0].lastUpdate;
+		separerdate();
+		console.log(date);
+
+	})
+	.catch(err => {
+		console.error(err);
+	});
+
+}
 
 
 
-fetch("https://covid-19-data.p.rapidapi.com/country?name=france", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-		"x-rapidapi-key": "2d024dd615msh1eab2c47260e3bfp1b9a61jsn10d150aa6b43"
-	}
-})
-.then(response => {
-	console.log(response);
-  return response.json();
-})
-.then(function(object) {
-  console.log(object);
-  cas = object[0].confirmed;
-  boxcas.textContent = cas;
-  morts = object[0].deaths;
-  boxmorts.textContent = morts;
-  hospital = object[0].critical;
-  boxhospital.textContent = hospital;
-
-  hospital = object[0].confirmed;
-  console.log(cas);
-})
-.catch(err => {
-	console.error(err);
-});
+function separerdate() {
+	var tab = date.split('-');
+	annee = tab[0];
+	mois = tab[1];
+	jour = tab[2].substr(0,2);
+	console.log(annee);
+	console.log(mois);
+	console.log(jour);
+	boxdate.textContent = jour + " / " + mois + " / " + annee;
+}
